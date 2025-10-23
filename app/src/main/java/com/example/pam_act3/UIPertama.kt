21e899de5942a692f5e05ac96863d99e598e2671
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-23 15:59:23 +0700

    tambahkan layout Column dengan padding dan ukuran penuh

--- app/src/main/java/com/example/pam_act3/UIPertama.kt
+++ app/src/main/java/com/example/pam_act3/UIPertama.kt
@@ -29,4 +29,10 @@ import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun ActivitasPertama(modifier: Modifier){}
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier= Modifier
+        .padding(top=100.dp)
+        .fillMaxSize(),
+
+    ){}
+}
